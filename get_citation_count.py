c6facb55a6502b2ab420229af449297cd5e7bd97
Chao He <huantaihechao@163.com>
2023-10-06 21:12:23 +0800

    Update get_citation_count.py

--- get_citation_count.py
+++ get_citation_count.py
@@ -1,14 +1,14 @@
-import requests
+import scholarly
 
-def get_citation_count(doi):
-    url = f"https://api.crossref.org/works/{doi}"
-    response = requests.get(url)
-    data = response.json()
-    citation_count = data['message']['reference-count']
+def get_citation_count(title):
+    search_query = scholarly.search_pubs(title)
+    publication = next(search_query)
+    citation_count = publication.citedby
     return citation_count
 
 if __name__ == "__main__":
-    doi = "10.1016/j.measurement.2021.110242"
-    citation_count = get_citation_count(doi)
-    print(f"Paper DOI: {doi}")
+    paper_title = "YOUR_PAPER_TITLE_HERE"
+    citation_count = get_citation_count(paper_title)
+    print(f"Paper Title: {paper_title}")
     print(f"Citation Count: {citation_count}")
+
